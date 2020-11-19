079d79120adc860559189ff0435f494ee524e982
Benjamin Chaiken <bfchaiken@gmail.com>
2020-11-19 09:05:02 -0800

    Fixed problem with adding results

--- Stock_cutting_margins.py
+++ Stock_cutting_margins.py
@@ -314,11 +314,10 @@ def stock_cutting(request):
 			
 				#### Seems to be ok to do this in place like this...
 				EPL = [EPL[order_i[j]] for j in range(len(order_i))]
-				# added in the margin on the outside of the sheet to the x and y 
-				# i.e. shift things by (1,1) to give the location on the original sheet 
-				Result = [ptp[p][2], {'part': ptp[p][0], 'x': Cr_EPs[k][L-1][0] + margin , 'y': Cr_EPs[k][L-1][1]+ margin + (5+Actual_dim[1])*packed_in}]
+			# added in the margin on the outside of the sheet to the x and y 
+			# i.e. shift things by (1,1) to give the location on the original sheet 
+			Result = [ptp[p][2], {'part': ptp[p][0], 'x': Cr_EPs[k][L-1][0] + margin , 'y': Cr_EPs[k][L-1][1]+ margin + (5+Actual_dim[1])*packed_in}]
 		
-	
 			Packings.append(Result)
 		
 			SH[k] = EPL 	
